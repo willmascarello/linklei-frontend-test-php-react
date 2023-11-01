2537bf0bf60007ea65b6b83f2733c8885ba6b55b
William Mascarello <wcrmascarello@gmail.com>
2023-11-01 17:39:11 -0300

    feat(main): modal crate post

--- linklei-frontend-test-react/src/components/create-post/create-post.tsx
+++ linklei-frontend-test-react/src/components/create-post/create-post.tsx
@@ -1,13 +1,51 @@
-import { Button } from "react-bootstrap";
+import { Button, Form, Modal } from "react-bootstrap";
 import { CreatePostStyled } from "./create-post.style";
+import { useState } from "react";
 
 export function CreatePost() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <>
       <CreatePostStyled>
-        <Button variant="primary">Criar post</Button>{" "}
-        {/* TODO: add onClick, open modal */}
+        <Button variant="primary" onClick={handleShow}>
+          Criar post
+        </Button>
       </CreatePostStyled>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Criar post</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="name">
+              <Form.Control type="name" placeholder="Autor do post" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="type">
+              <Form.Select aria-label="Default select example">
+                <option disabled selected>
+                  Selecione o tipo do post
+                </option>
+                <option value="post">Post</option>
+                <option value="artigo">Artigo</option>
+                <option value="grupo">Grupo</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="textarea">
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            Publicar
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
